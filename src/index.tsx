1775a43e5ddb482dbb81260eaf339d815bcfa55f
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-15 19:25:52 +0300

    add react routing package and edit index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+const app = (
+    <BrowserRouter><App/></BrowserRouter>
+)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      {app}
   </React.StrictMode>
 );
 
