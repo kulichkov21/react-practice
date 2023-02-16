7c57a5d36759c910b0ea7a8a4a504f032fd4c251
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-16 20:14:32 +0300

    config quiz list

--- src/components/FinishedQuiz/FinishedQuiz.tsx
+++ src/components/FinishedQuiz/FinishedQuiz.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './FinishedQuiz.css';
 import Button from "../Ui/Button/Button";
+import {Link} from "react-router-dom";
 
 const FinishedQuiz = (props: any) => {
     const successCount = Object.keys(props.results).reduce((total: any, key: any) => {
@@ -28,7 +29,10 @@ const FinishedQuiz = (props: any) => {
             <p>Right answers: {successCount} of {props.quiz.length}</p>
             <div>
          <Button onClick={props.onRetry} type='primary'>Repeat</Button>
-                <Button onClick={props.onRetry} type='success'>Go to all tests</Button>
+                <Link to={'/'}>
+                    <Button onClick={props.onRetry} type='success'>Go to all tests</Button>
+                </Link>
+
             </div>
         </div>
     )
