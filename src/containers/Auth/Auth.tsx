2417a97e22b1452dc73aa57d6b5bb2975e19bc5c
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-17 13:53:30 +0300

    config quiz list

--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -1,11 +1,37 @@
 import React, {Component} from "react";
+import './Auth.css';
+import Button from "../../components/Ui/Button/Button";
 
 export default class Auth extends Component<any, any> {
 
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = (event:any) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className='Auth'>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.submitHandler} className='AuthForm'>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <Button type='success' onClick={this.loginHandler}>
+                            Войти
+                        </Button>
+                        <Button type='primary' onClick={this.registerHandler}>
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
