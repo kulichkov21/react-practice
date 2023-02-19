49db8703670d9687426d0f407e41fecdb6476741
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-19 18:57:36 +0300

    add axios package

--- src/containers/QuizList/QuizList.tsx
+++ src/containers/QuizList/QuizList.tsx
@@ -1,17 +1,22 @@
 import React, {Component} from "react";
 import './QuizList.css';
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 export default class QuizList extends Component<any, any> {
 
     renderQuizes() {
-       return [1, 2, 3].map((quiz, index) => {
-           return (
-               <li key={index}>
-                  <NavLink to={'/quiz/' + quiz}>Test {quiz}</NavLink>
-               </li>
-           )
-       })
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li key={index}>
+                    <NavLink to={'/quiz/' + quiz}>Test {quiz}</NavLink>
+                </li>
+            )
+        })
+    }
+
+    componentDidMount() {
+        axios.get('https://react-quiz-5eab9-default-rtdb.firebaseio.com/quiz.json').then(r => console.log(r))
     }
 
     render() {
